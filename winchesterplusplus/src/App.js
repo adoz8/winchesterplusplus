aa35a447d7ac9d2e71c8411ca0a61c4a62bc26a8
Byron Lloyd Wakeman <byzstorm1103@gmail.com>
2022-12-01 17:39:37 +0000

    Created routes for everyones pages by name

--- winchesterplusplus/src/App.js
+++ winchesterplusplus/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./Pages/home";
+import Alisa from "./Pages/alisa";
+import Byron from "./Pages/byron";
+import Rachitha from "./Pages/rachitha";
+import Salmah from "./Pages/salmah";
+
+import { Component } from 'react';
+import { Route } from "react-router-dom"
+
+class App extends Component {
+  render()
+  {
+    return(
+      <div>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/alisa" component={Alisa}/>
+        <Route exact path="/byron" component={Byron}/>
+        <Route exact path="/rachitha" component={Rachitha}/>
+        <Route exact path="/salmah" component={Salmah}/>
+      </div>
+    )
+  }
 }
 
 export default App;
