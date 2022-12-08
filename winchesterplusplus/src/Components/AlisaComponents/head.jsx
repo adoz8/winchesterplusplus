ca068709ed3a670a9a34331a36af97a25bb2f1a5
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-08 13:11:51 +0000

    08122022

---
+++ winchesterplusplus/src/Components/AlisaComponents/head.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./aboutMe.css";
+
+const Head = () => {
+    return(
+        <h1 className="aboutMe">About Me....</h1>
+    )
+}
+
+export default Head;
--- winchesterplusplus/src/Pages/alisa.js
+++ winchesterplusplus/src/Pages/alisa.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Head from "winchesterplusplus/src/Components/AlisaComponents/head.jsx;"; 
 
 const Alisa = () => {
     return(
         <div>
             <h1>Alisa's page</h1>
+            <div>
+                <Head/>
+            </div>
         </div>
+        
     )
 }
 
