d40bd8993ffee3f186df16e5de45c3b6e6c52346
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-08 15:52:08 +0000

    trying to add a cat

--- winchesterplusplus/src/Components/AlisaComponents/head.jsx
+++ winchesterplusplus/src/Components/AlisaComponents/head.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./cat.css";
 import './head.css';
 const Head = () => {
     return(
--- winchesterplusplus/src/Pages/alisa.js
+++ winchesterplusplus/src/Pages/alisa.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "../Components/AlisaComponents/head";
+import CatHands from "https://s3-us-west-2.amazonaws.com/s.cdpn.io/59639/bundle_copy.js";
 
 const Alisa = () => {
     return(
@@ -7,6 +8,8 @@ const Alisa = () => {
             <h1>Alisa's page</h1>
             <div>
                 <Head/>
+                CatHands.start(document.body, {'Click & Hold'
+                });
             </div>
         </div>
         
