031a9c14d99499bdb37984891988d32eb958e69f
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-09 12:28:40 +0000

    image please

--- winchesterplusplus/src/Pages/alisa.js
+++ winchesterplusplus/src/Pages/alisa.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Head from "../Components/AlisaComponents/head";
+import Button from "../Components/AlisaComponents/button"
 
 const Alisa = () => {
     return(
         <div>
             <div>
                 <Head/>
+            </div>
+            <div>
+                <Button/>
             </div>
         </div>
         
