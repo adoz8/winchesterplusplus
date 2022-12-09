107b9dc0957c45531c1272cac322a73a880ffaa3
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-09 12:37:58 +0000

    cat shrinks

--- winchesterplusplus/src/Components/AlisaComponents/button.jsx
+++ winchesterplusplus/src/Components/AlisaComponents/button.jsx
@@ -4,8 +4,10 @@ import Cat from "./Cat.jpg"
 const Button = () => {
     return(
         <div class="container">
+            <div>
             <h1>Cat</h1>
             <img calss = "ct" alt="Cat" src ={Cat}/>
+            </div>
         </div>
     )
 }
