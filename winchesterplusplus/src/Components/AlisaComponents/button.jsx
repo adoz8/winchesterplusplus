2600c3aef7794ea496487085821b06dd62039c6c
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-09 12:25:59 +0000

    image

---
+++ winchesterplusplus/src/Components/AlisaComponents/button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './button.css';
+import "./Cat.jpg"
+const Button = () => {
+    return(
+        <div class="container">
+            <h1>Cat</h1>
+            <img alt="Cat" src ="../Cat.jpg"/>
+        </div>
+    )
+}
+
+export default Button;
