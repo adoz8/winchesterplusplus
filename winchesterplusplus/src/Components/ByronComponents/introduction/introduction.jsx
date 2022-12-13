8fa6baf44f5455d5d4c37c3b10b89df653d14f20
Byron Lloyd Wakeman <byzstorm1103@gmail.com>
2022-12-13 17:24:49 +0000

    Added bongocat animation

--- winchesterplusplus/src/Components/ByronComponents/introduction/introduction.jsx
+++ winchesterplusplus/src/Components/ByronComponents/introduction/introduction.jsx
@@ -3,13 +3,23 @@ import "./title/title.css";
 
 import Title from "./title/title";
 import Description from "./description/descrption";
+import BongoCatRight from "./bongocat/bongocatright";
+import BongoCatLeft from "./bongocat/bongocatleft";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Introduction = () =>
 {
+    useState(() => {
+        console.log("hello")
+    }, [])
+
     return(
         <div className="introduction">
             <Title/>
             <Description/>
+            <BongoCatLeft/>
+            <BongoCatRight/>
         </div>
     )
 }
