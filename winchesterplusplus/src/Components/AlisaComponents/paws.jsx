a775195d652020e5c3f2999bd5d605447a8ff5b2
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-13 14:24:49 +0000

    ???

--- winchesterplusplus/src/Components/AlisaComponents/paws.jsx
+++ winchesterplusplus/src/Components/AlisaComponents/paws.jsx
@@ -1,22 +1,8 @@
-import CatHands from "../Components/AlisaComponents/catpawsnew";
-<>
-  <meta charSet="UTF-8" />
-  <title>CodePen - Cat Hands</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link
-    rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"
-  />
-  <link
-    rel="stylesheet"
-    href="https://codepen.io/jonny/pen/jQvLEr/b3e4dc7bad564ec9e18b5b0928f75587.css"
-  />
-  <link rel="stylesheet" href="./style.css" />
-  {/* partial:index.partial.html */}
-  {/* partial */}
-</>
+import "https://s3-us-west-2.amazonaws.com/s.cdpn.io/59639/bundle_copy.js";
+
 
 const Paws = () =>{
+    //eslint-disable-next-line
     CatHands.start(document.body, {
 
         title: 'Click & Hold'
