6911fb5d71e6f3247355b576fa725713b3bf7c22
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-13 13:19:59 +0000

    cat paws jsx

--- winchesterplusplus/src/Components/AlisaComponents/paws.jsx
+++ winchesterplusplus/src/Components/AlisaComponents/paws.jsx
@@ -1,5 +1,19 @@
-import CatHands from "https://s3-us-west-2.amazonaws.com/s.cdpn.io/59639/bundle_copy.js";
-import "./paws.css";
+<>
+  <meta charSet="UTF-8" />
+  <title>CodePen - Cat Hands</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"
+  />
+  <link
+    rel="stylesheet"
+    href="https://codepen.io/jonny/pen/jQvLEr/b3e4dc7bad564ec9e18b5b0928f75587.css"
+  />
+  <link rel="stylesheet" href="./style.css" />
+  {/* partial:index.partial.html */}
+  {/* partial */}
+</>
 
 const Paws = () =>{
     CatHands.start(document.body, {
