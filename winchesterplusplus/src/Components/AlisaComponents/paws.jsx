b42c007cde4db61256bb1082decb0086aa3c40ca
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-13 11:37:26 +0000

    catt

---
+++ winchesterplusplus/src/Components/AlisaComponents/paws.jsx
@@ -0,0 +1,11 @@
+import CatHands from "https://s3-us-west-2.amazonaws.com/s.cdpn.io/59639/bundle_copy.js";
+import "../paws.css";
+
+const Paws = () =>{
+    CatHands.start(document.body, {
+
+        title: 'Click & Hold'
+    });
+}
+
+export default Paws;
