c4c499f9e95c678728f266e9b4fe4567b22f6dc3
Byron Lloyd Wakeman <byzstorm1103@gmail.com>
2022-12-14 10:03:10 +0000

    Added dog and map images in carousel

--- winchesterplusplus/src/Components/ByronComponents/aboutMe/aboutMe.jsx
+++ winchesterplusplus/src/Components/ByronComponents/aboutMe/aboutMe.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import "./aboutMe.css";
 import { Component } from "react";
 import Carousel from "./carousel/carousel";
+import Map from "./map";
 
 const Slide1 = () => {
     return (
         <div className="carousel__item">
             <h1 className="aboutMe__title">About Me</h1> 
-            <h2 className="description">Some interesting facts about me...
-            </h2>
+            <ul className="description aboutMe__list-items">
+                <li>I'm 19</li>
+                <li>From stafford</li>
+                <li>Worked at two warehouses before Fujitsu (glad to be here)</li>
+                <div className="map-img__container">
+                    <Map/>
+                </div>
+            </ul>
         </div>
     )
 }
@@ -16,8 +23,9 @@ const Slide1 = () => {
 const Slide2 = () => {
     return (
         <div className="carousel__item">
-            <h1 className="aboutMe__title">Slide 2</h1> 
-            <h2 className="description">slide 2's description</h2>
+            <h1 className="aboutMe__title">My dog</h1> 
+            <h2 className="description">Got a pet dog called freya</h2>
+            <img className="freya__img" src={require("./freya.jpg")}></img>
         </div>
     )
 }
