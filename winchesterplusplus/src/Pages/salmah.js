7741a4d959562f442ff44823b7b892449ccd00bb
Salmah127 <119612140+Salmah127@users.noreply.github.com>
2022-12-14 19:40:58 +0000

    Salmah- filled cards

--- winchesterplusplus/src/Pages/salmah.js
+++ winchesterplusplus/src/Pages/salmah.js
@@ -17,9 +17,19 @@ const Salmah = () => {
             </div>
             <div className="InfoCards">
                 <Card
-                title = "Card Title"
-                imageURL = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fflowers%2F&psig=AOvVaw20oY8yZicbnuEVQ_wHtaEY&ust=1671129081851000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCODz4Yvf-fsCFQAAAAAdAAAAABAE"
-                body = "Testing if it works"
+                title = "Born in Australia"
+                imageURL = "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.state.gov%2Fcountries-areas%2Faustralia%2F&psig=AOvVaw3xr6xtLgqH1FLYC-elA05W&ust=1671132582070000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCKi8uJHs-fsCFQAAAAAdAAAAABAE"
+                body = "I was born in Australia but only lived there for 6 months"
+                />
+                <Card
+                title = "Nationality is Sri Lankan"
+                imageURL = "https://www.google.co.uk/url?sa=i&url=https%3A%2F%2Fwww.travelnation.co.uk%2Fdestinations%2Fsri-lanka%2Fsri-lanka-travel-guide&psig=AOvVaw1GRbCk9UM1F5jGPTxbcPDa&ust=1671132656539000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPjE-LTs-fsCFQAAAAAdAAAAABAE"
+                body = "My nationality is Sri Lankan, but I 'act' like the least Sri Lankan in my family"
+                />
+                <Card
+                title= "Lived in the UK for 16 years"
+                imageURL ="https://www.whereig.com/united-kingdom/maps/leicester-location-map.jpg"
+                body= "I have lived in Reading and live in Leicester - East Midlands"
                 />
             </div>
         </div>
