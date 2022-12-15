3359f7f6dca4d3c3d6ece4489d9c2b6b4eea6cfe
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-15 14:08:45 +0000

    ===

--- winchesterplusplus/src/Pages/salmah.js
+++ winchesterplusplus/src/Pages/salmah.js
@@ -1,26 +1,26 @@
 /* eslint-disable react/jsx-no-duplicate-props */
 /* eslint-disable no-unused-vars */
 import React from "react";
-import '../Components/SalmahComponents/MainStyleSheet.css';
+import "../Components/SalmahComponents/MainStyleSheet.css";
 
 import Button from "../Components/SalmahComponents/BackButton/BackButton";
 import TypeAnimation from "../Components/SalmahComponents/TypeAnimation/TypeAnimation";
 import ColourSwitcher from "../Components/SalmahComponents/ColourSwitch/colour-switch";
-import Card from "../Components/SalmahComponents/Card/card";
+// import Card from "../Components/SalmahComponents/Card/card";
 
 const Salmah = () => {
-    return(
-        <div className="SalmahPage">
-            <div className="Intro">
-                <Button />
-                <ColourSwitcher />
-                <TypeAnimation /> 
-            </div>
-            <div className="Cards">
-                <card />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="SalmahPage">
+      <div className="Intro">
+        <Button />
+        <ColourSwitcher />
+        <TypeAnimation />
+      </div>
+      <div className="Cards">
+        <card />
+      </div>
+    </div>
+  );
+};
 
 export default Salmah;
