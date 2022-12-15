4c6b68b34eed43280e29043505c66310808ce33f
Byron Lloyd Wakeman <byzstorm1103@gmail.com>
2022-12-15 21:52:24 +0000

    Created walking cat element which walks across home page screen

--- winchesterplusplus/src/Pages/home.js
+++ winchesterplusplus/src/Pages/home.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom"
 
+import "../Components/HomeComponents/background.css";
+import WalkingCat from "../Components/HomeComponents/WalkingCat/walkingcat";
+
 const Home = () => {
     return(
-        <div>
-            <Link to="/alisa">Alisa's page</Link>
+        <div className="home-background__container">
+            {/* <Link to="/alisa">Alisa's page</Link>
             <br/>
             <Link to="/byron">Byron's page</Link>
             <br/>
             <Link to="/rachitha">Rachitha's page</Link>
             <br/>
-            <Link to="/salmah">Salmah's page</Link>
+            <Link to="/salmah">Salmah's page</Link> */}
+            <WalkingCat/>
         </div>
     )
 }
