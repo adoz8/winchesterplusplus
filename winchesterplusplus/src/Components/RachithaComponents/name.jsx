656fc098e9e58ad6d9acfd4c0dfd2ccda37cf10e
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-16 15:00:41 +0000

    rachita

--- winchesterplusplus/src/Components/RachithaComponents/name.jsx
+++ winchesterplusplus/src/Components/RachithaComponents/name.jsx
@@ -1,29 +1,17 @@
 import React from "react";
 import "./css/style.css";
-<<<<<<< HEAD
-=======
 import "./css/buttonfun.css";
-<<<<<<< HEAD
->>>>>>> debda1beff5ffd8ea95ca12d8bc4783a22ef4334
-=======
 import { Link } from "react-router-dom";
->>>>>>> 803d01ef347e7b5f4b344f1134ac58f0923ed3d0
 
 const Rachiname = () =>
 {
     return(
         <div>
-<<<<<<< HEAD
-<<<<<<< HEAD
             <h1 className="rachitha-text">Hi My name is Rachitha</h1>
-=======
             <h1 className="rachiname">Hi My name is Rachitha</h1>
->>>>>>> debda1beff5ffd8ea95ca12d8bc4783a22ef4334
-=======
             {/* <Link to "/rachitha2"> */}
             <h1 className="rachiname">Hi My name is Rachitha</h1>
             {/* </Link> */}
->>>>>>> 803d01ef347e7b5f4b344f1134ac58f0923ed3d0
         </div>
     );
 }
