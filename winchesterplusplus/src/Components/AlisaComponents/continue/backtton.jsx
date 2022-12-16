b871f4076c155ce8b7f7a5d12e0d15611ec42c2d
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-16 13:00:44 +0000

    button

---
+++ winchesterplusplus/src/Components/AlisaComponents/continue/backtton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './backtton.css';
+import { Link } from "react-router-dom"
+const Backtton = () => {
+    return(
+        <div class="backtton">
+            <div>
+                <a href="/"><h1>Walk in the park</h1></a>
+            </div>
+        </div>
+    )
+}
+
+export default Backtton;
