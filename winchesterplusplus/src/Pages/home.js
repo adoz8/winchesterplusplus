3fdc7f4cc9bfe36359d59795ef3f6ecd9323b908
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-16 14:57:06 +0000

    ???

--- winchesterplusplus/src/Pages/home.js
+++ winchesterplusplus/src/Pages/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 import "../Components/HomeComponents/background.css";
 import SpinningSun from "../Components/HomeComponents/SpinningSun/SpinningSun";
@@ -8,24 +8,20 @@ import WalkingCat from "../Components/HomeComponents/WalkingCat/walkingcat";
 // import sunflower2 from "../Components/RachithaComponents/sunflower";
 
 const Home = () => {
-    return(
-        <div className="home-background__container">
-            {/* <Link to="/alisa">Alisa's page</Link>
+  return (
+    <div className="home-background__container">
+      {/* <Link to="/alisa">Alisa's page</Link>
             <br/>
             <Link to="/byron">Byron's page</Link>
             <br/>
             <Link to="/rachitha">Rachitha's page</Link>
             <br/>
             <Link to="/salmah">Salmah's page</Link> */}
-            <StandingDog/>
-            <WalkingCat/>
-<<<<<<< HEAD
-            // <sunflower2/>
-=======
-            <SpinningSun />
->>>>>>> debda1beff5ffd8ea95ca12d8bc4783a22ef4334
-        </div>
-    )
-}
+      <StandingDog />
+      <WalkingCat />
+      // <sunflower2 />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
