3e564707fd099b0f2de34a35a939ce35b8fcfc49
Byron Lloyd Wakeman <byzstorm1103@gmail.com>
2022-12-19 10:52:00 +0000

    Merged with previous version that was overwritten

---
+++ winchesterplusplus/src/Pages/home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "../Components/HomeComponents/background.css";
+import SpinningSun from "../Components/HomeComponents/SpinningSun/SpinningSun";
+import StandingDog from "../Components/HomeComponents/StandingDog/standingdog";
+import WalkingCat from "../Components/HomeComponents/WalkingCat/walkingcat";
+import Sunflower2 from "../Components/RachithaComponents/sunflower";
+
+const Home = () => {
+  return (
+    <div className="home-background__container">
+      {/* <Link to="/alisa">Alisa's page</Link>
+        <br/>
+        <Link to="/byron">Byron's page</Link>
+        <br/>
+        <Link to="/rachitha">Rachitha's page</Link>
+        <br/>
+        <Link to="/salmah">Salmah's page</Link> */}
+      <SpinningSun />
+      <StandingDog />
+      <WalkingCat />
+      <Sunflower2/>
+
+    </div>
+  );
+};
+
+export default Home;
