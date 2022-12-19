0a6fc52f7226cdcaca857be12639f0ed201a2f4c
Byron Lloyd Wakeman <byzstorm1103@gmail.com>
2022-12-19 17:38:07 +0000

    Created night/day toggle button

--- winchesterplusplus/src/Pages/home.js
+++ winchesterplusplus/src/Pages/home.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 import "../Components/HomeComponents/background.css";
+import "./nightdaybutton.css";
+
 import SpinningSun from "../Components/HomeComponents/SpinningSun/SpinningSun";
 import SpinningMoon from "../Components/HomeComponents/SpinningSun/SpinningMoon";
-
 import StandingDog from "../Components/HomeComponents/StandingDog/standingdog";
 import WalkingCat from "../Components/HomeComponents/WalkingCat/walkingcat";
 import Sunflower2 from "../Components/RachithaComponents/sunflower";
 
 const Home = () => {
-    const [isNight, setIsNight] = useState(false)
-
+    const [isNight, setIsNight] = useState(false);
     return (
       <div id="background" className="home-background__container">
         {/* <Link to="/alisa">Alisa's page</Link>
@@ -27,25 +27,25 @@ const Home = () => {
         <StandingDog />
         <WalkingCat />
         <Sunflower2/>
-        <button onClick={() =>
-        {
-            var background = document.getElementById("background");
-            if(isNight)
+          <input className="nightdayinput" type="checkbox" id="night-toggle"/>
+          <label onClick={() =>
             {
-              background.classList.remove("home-night-background__container");
-              background.classList.add("home-background__container");
-              setIsNight(false)
-            }
-            else
-            {
-              background.classList.add("home-night-background__container");
-              background.classList.remove("home-background__container");
-              setIsNight(true);
-            }
-            isNight ^= true;
-  
-        }}>Change</button>
-  
+                var background = document.getElementById("background");
+                if(isNight)
+                {
+                  background.classList.remove("home-night-background__container");
+                  background.classList.add("home-background__container");
+                  setIsNight(false)
+                }
+                else
+                {
+                  background.classList.add("home-night-background__container");
+                  background.classList.remove("home-background__container");
+                  setIsNight(true);
+                }
+                isNight ^= true;
+      
+            }} className="nightlabel" for="night-toggle"></label>
       </div>
     );
 
