9c8dabf4b480d397400d0c9aa0ceb0779557e8d4
Salmah127 <119612140+Salmah127@users.noreply.github.com>
2022-12-20 12:30:24 +0000

    1st set of cards done

--- winchesterplusplus/src/Pages/salmah.js
+++ winchesterplusplus/src/Pages/salmah.js
@@ -5,7 +5,8 @@ import "../Components/SalmahComponents/MainStyleSheet.css";
 import Button from "../Components/SalmahComponents/Button";
 import ColourSwitcher from "../Components/SalmahComponents/ColourSwitch/colour-switch";
 import TypeAnimation from "../Components/SalmahComponents/TypeAnimation/TypeAnimation";
-import GroupExample from "../Components/SalmahComponents/CardGroup/CardGroup";
+import Cards from "../Components/SalmahComponents/Cards/Cards";
+
 
 
 
@@ -21,6 +22,30 @@ const Salmah = () => {
       </div>
       <div className="Subheading">
         <TypeAnimation />
+      </div>
+      <div className="Card_Wrapper">
+        <Cards
+        altext="Victoria Melbourne"
+        image={require("../Components/SalmahComponents/Cards/melbourne.png")}
+        title="Born in?"
+        content="I was born in Australia, Melbourne.
+        But I only lived there for 6 months."
+        />
+        <Cards
+        altext="Sri Lanka Map"
+        image={require("../Components/SalmahComponents/Cards/srilanka.png")}
+        title="My Nationality is?"
+        content="My Nationality is Sri Lankan however I have only lived there for around 4-5 years.
+        I am the least Sri Lankan in my family and cannot stand spice."
+        />
+        <Cards
+        altext="Narborough Road"
+        image={require("../Components/SalmahComponents/Cards/leicester.png")}
+        title="Where do I live now?"
+        content="I have been living in Leicester since 2008.
+        I used to live in Reading when I first moved to the UK in 2006, so not too far from the Bracknell Office.
+        Did you know Leicester has UK's most diverse street with shopkeepers coming from 23 different countries!"
+        />
       </div>
     </div>
   );
