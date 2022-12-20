486ee19429466f7e82ad2457a19fd2c2acb7e245
RachithaAdikari <119612169+RachithaAdikari@users.noreply.github.com>
2022-12-20 00:19:08 +0000

    second page Please

--- winchesterplusplus/src/Pages/rachitha2.js
+++ winchesterplusplus/src/Pages/rachitha2.js
@@ -1,15 +1,21 @@
 import React from "react";
-import "../Components/RachithaComponents/css/rachitha2.css";
-import Camera2 from "../Components/RachithaComponents/camera";
+import "../Components/RachithaComponents/css/Rachi2.css";
+// import Potty from "../Components/RachithaComponents/potplant";
+import { SliderData2 } from "../Components/RachithaComponents/SliderData";
+import ImageSlider from "../Components/RachithaComponents/ImageSlider";
 
+// import { SliderData2 } from './SliderData';
 
-const Rachitha2 = () => {
-    return(
+function Apple(){
+   return <ImageSlider slides={SliderData2} />;
+}
+// const Rachitha2 = () => {
+//     return(
 
-         <div className="rachitha2page" >
-            <Camera2/>
-         </div>
-    );
- }
+//          <div className="rachitha2page" >
+//             <Potty />
+//          </div>
+//     );
+//  }
 
- export default Rachitha2;
+ export default Apple;
