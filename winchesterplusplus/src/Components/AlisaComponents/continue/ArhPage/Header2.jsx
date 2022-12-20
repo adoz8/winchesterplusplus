0a43821faa751dafe49fcbbeef0fdf8eec23d154
adoz8 <alisa.dozorceva@fujitsu.com>
2022-12-20 14:34:51 +0000

    text update

--- winchesterplusplus/src/Components/AlisaComponents/continue/ArhPage/Header2.jsx
+++ winchesterplusplus/src/Components/AlisaComponents/continue/ArhPage/Header2.jsx
@@ -3,9 +3,9 @@ import './head.css';
 const HeadArh = () => {
     return(
         <header>
-            <h3 class ="tiny">What i did for 5 years at 3 am in pure panick injecting redbull intpo my eyes.
+            <h3 class ="tiny">What i did for 5 years at 3 am in pure panick injecting redbull intpo my eyes. I mean...
             </h3>
-            <h1>I mean Architecture</h1>
+            <h1>...Architecture</h1>
         </header>
     )
 }
