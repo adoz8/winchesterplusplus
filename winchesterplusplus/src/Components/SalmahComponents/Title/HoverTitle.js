8d1e53eac706d9d4951dc04435d5c88e4f541e55
Salmah127 <119612140+Salmah127@users.noreply.github.com>
2023-01-15 18:06:18 +0000

    hover title

---
+++ winchesterplusplus/src/Components/SalmahComponents/Title/HoverTitle.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import './HoverTitle.css';
+
+
+function HoverTitle() {
+  return (
+    <div class="center-container">
+        <span class="container">
+            <div class="text" data-text="SalmahShihamAbdulwahid">Salmah Shiham Abdulwahid
+            </div>
+        </span>
+    </div>
+  )
+}
+
+export default HoverTitle;
+
