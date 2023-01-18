758388309d511b3aadad916d4374b15950b8e8ac
RachithaAdikari <119612169+RachithaAdikari@users.noreply.github.com>
2023-01-18 10:39:24 +0000

    no change

--- winchesterplusplus/src/Components/RachithaComponents/ImageSlider.js
+++ winchesterplusplus/src/Components/RachithaComponents/ImageSlider.js
@@ -2,8 +2,8 @@ import React, {useState} from 'react';
 import { SliderData2 } from './SliderData';
 import {FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa'
 
-const ImageD = ['./images/sl.jpg', './images/buddhism.jpg', './images/logos.jpg', './images/dilini.jpg','./images/sripada.jpg','./images/butterfly.jpg','./images/cake1.jpg']
-const ImageText = ["I'm from Sri Lanka","I am passionate about Buddhism","Charitable work and volunteering is a big part of me", "Dilini's family is my second family","I enjoy hiking","I am passionate about sustainability", "I love baking"]
+const ImageD = ['./images/sl.jpg', './images/buddhism.jpg', './images/logos.jpg', './images/sripada.jpg','./images/stupa.jpg','./images/cake1.jpg']
+const ImageText = ["I'm from Sri Lanka","I am passionate about Buddhism","Charitable work and volunteering is a big part of me", "I enjoy hiking","...landscape photography", "...and baking"]
 const ImageSlider = ({ slides}) => {
 const [current, setCurrent]  = useState(0);
 const length = slides.length;
